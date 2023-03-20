89e9f9ec185bbd3dc45c1f28175a10e05d663d95
Malkavianson <63012624+Malkavianson@users.noreply.github.com>
2023-03-20 18:28:55 -0300

    Feat: sended to production (#2)
    * Build: add @eslint settings
    
    * Build: add @prettier settings
    
    * Build: update @typescript settings
    
    * Style: fix indentation
    
    * Feat: add redirect endpoint and doc controller
    
    * Build: update @eslint settings
    
    * gitignore
    
    * Feat: add @Prisma
    
    * Feat: define architeture
    
    * Feat: nest middleware decorator
    
    * Feat: add JWT file
    
    * Feat: add @prisma Schema
    
    * Fix: fixed custom scripts
    
    * Feat: Auth and User Endpoints
    
    * Fix: fix @JWT middleware sintax
    
    * .env
    
    * Fix: remove root dir path
    
    * Feat: category endpoint
    
    * Feat: add exceptions
    
    * Feat: add formatter
    
    * Feat: add list endpoint
    
    * Feat: add exception types
    
    * Fix: remove generic path
    
    * Feat: add swagger and CORS
    
    * Feat: add testing
    
    * Delete yarn.lock

---
+++ API/src/models/create-list.dto.ts
@@ -0,0 +1,17 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString } from "class-validator";
+
+export class ListDto {
+	@ApiProperty({ description: "list title", example: "kitchen" })
+	@IsNotEmpty()
+	@IsString()
+	title: string;
+
+	@ApiProperty({ description: "list text", example: "fruits and vegetables" })
+	@IsNotEmpty()
+	@IsString()
+	text: string;
+
+	@ApiProperty({ description: "category ID" })
+	categoryId?: string;
+}

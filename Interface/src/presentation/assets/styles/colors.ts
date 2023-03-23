9345de6ac5f826385c4ce3b33700d7a9cf478869
malkavianson <alexneto.best@gmail.com>
2023-03-23 16:53:28 -0300

    Feat: add assets and globals

---
+++ Interface/src/presentation/assets/styles/colors.ts
@@ -0,0 +1,22 @@
+export const colors = {
+	primaryColor: "#6CEA69",
+	secondaryColor: "#E26B6B",
+
+	contrast1: "#ffffff",
+	contrast0: "#000000",
+
+	primaryColorOpacity: "rgba(108, 234, 105, 0.4)",
+
+	shadowColor: "#6CEA69",
+
+	baseBg1: "#252836",
+	baseBg2: "#1F1D2B",
+	baseForm: "#2D303E",
+	baseLine: "#393C49",
+
+	textColor: "#ffffff",
+	textLighter: "#E0E6E9",
+	textLight: "#ABBBC2",
+	textNeutral: "#889898",
+	textDark: "#3B5162",
+};
